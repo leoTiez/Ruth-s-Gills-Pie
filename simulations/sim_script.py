d449d3054289156ac2105a5ab4af3d40f3aa631b
Leo Zeitler <leo.zeitler@yahoo.com>
2025-07-02 11:49:10 +0200

    Update: several trials
    repeat simulation over several trials to allow averaging. increased
    simulaiton time for singleStatic setup

--- simulations/sim_script.py
+++ simulations/sim_script.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from pathlib import Path
+import multiprocessing
 
 os.chdir('..')
 
@@ -12,6 +13,10 @@ def parse_args(args):
                             help='Simulation to be run.')
     arg_parser.add_argument('--trial_prefix', type=str, default='',
                             help='Prefix that can be set for trial.')
+    arg_parser.add_argument('--n_cpus', type=int, default=1,
+                            help='Number of cpus used.')
+    arg_parser.add_argument('--n_trials', type=int, default=10,
+                            help='Number of trials.')
     arg_parser.add_argument('--interpreter', type=str, default='python3.9',
                             help='Python interpreter to be used.')
     return arg_parser.parse_args(args)
@@ -20,16 +25,18 @@ def parse_args(args):
 def main(args):
     sim_type = args.sim_type
     trial_prefix = args.trial_prefix
+    n_cpus = args.n_cpus
+    n_trials = args.n_trials
     python_interpreter = args.interpreter
 
     if sim_type.lower() == 'single_static':
         sim_file = 'examples/singleStatic.py'
         n_cells = 100
-        sampling_time = 30
+        sampling_time = 130
     elif sim_type.lower() == 'single_force':
         sim_file = 'examples/singleForce.py'
         n_cells = 20
-        sampling_time = 300
+        sampling_time = 500
     elif sim_type.lower() == 'double_force':
         sim_file = 'examples/doubleForce.py'
         n_cells = 100
@@ -43,33 +50,36 @@ def main(args):
     uncertainty = 100
 
     Path('logs').mkdir(exist_ok=True, parents=True)
-    os.system(
-        '%s gSimulation.py '
-        '--simulation_file=%s '
-        '--save_sim_result '
-        '--save_fig '
-        '--verbosity=%d '
-        '--smoothing=%d '
-        '--n_samples=%d ' 
-        '--uncertainty=%d '
-        '--sampling_time=%.2f '
-        '--n_cells=%d '
-        '--trial_prefix=%s%s '
-        '--do_train=False '
-        '1> logs/out_%s%s.out 2> logs/err_%s%s.err' % (
-            python_interpreter,
-            sim_file,
-            verbosity,
-            smoothing,
-            n_samples,
-            uncertainty,
-            sampling_time,
-            n_cells,
-            trial_prefix, sim_type,
-            trial_prefix, sim_type,
-            trial_prefix, sim_type
-        )
-    )
+    with multiprocessing.Pool(processes=n_cpus) as parallel:
+        for i_trial in range(n_trials):
+            parallel.apply_async(os.system, args=(
+                '%s gSimulation.py '
+                '--simulation_file=%s_%d '
+                '--save_sim_result '
+                '--save_fig '
+                '--verbosity=%d '
+                '--smoothing=%d '
+                '--n_samples=%d ' 
+                '--uncertainty=%d '
+                '--sampling_time=%.2f '
+                '--n_cells=%d '
+                '--trial_prefix=%s%s%d '
+                '--do_train=False '
+                '1> logs/out_%s%s%d.out 2> logs/err_%s%s%d.err' % (
+                    python_interpreter,
+                    sim_file,
+                    i_trial,
+                    verbosity,
+                    smoothing,
+                    n_samples,
+                    uncertainty,
+                    sampling_time,
+                    n_cells,
+                    trial_prefix, sim_type, i_trial,
+                    trial_prefix, sim_type, i_trial,
+                    trial_prefix, sim_type, i_trial
+                ),
+            ))
 
 
 if __name__ == '__main__':
